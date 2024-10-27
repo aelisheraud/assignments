d550c8fb82cd6deb9d28ea547e7d7912edcc5142
aelis <aelis_heraud@hotmail.com>
2024-10-26 21:33:32 -0400

    Update the style of this module

--- 2_style/21_arload.py
+++ 2_style/21_arload.py
@@ -1,14 +1,37 @@
-import os, re, sys, shutil, getpass, datetime, subprocess
-from arUtil import ArUtil
-from tank import Tank
-import libLog, libData, libFunc
-import arNotice
+# Python Packages
+import os
+import re
+import sys
+import shutil
+import getpass
+import datetime
+import subprocess
+
+# Third Party Python Packages
 from Qt import QtWidgets, QtGui, QtCore, QtCompat
+
+# Custom Packages
+import libLog
+import libData
+import libFunc
+import arNotice
+
+from tank import Tank
+from arUtil import ArUtil
+
+
+#################################################################################
+# VARIABLES
 
 TITLE = "load"
 LOG = libLog.init(script=TITLE)
 
+
+#################################################################################
+# CLASSES & METHODS SETUP
+
 class ArLoad(ArUtil):
+
     def __init__(self):
         super(ArLoad, self).__init__()
         path_ui = ("/").join([os.path.dirname(__file__), "ui", TITLE + ".ui"])
@@ -24,61 +47,106 @@ class ArLoad(ArUtil):
         self.resize_widget(self.wgLoad)
         self.wgLoad.show()
         LOG.info('START : ArLoad')
+
+
+    ##Class:ArLoad************************************************************
+    # SETUP METHODS # 
+    
+    def fill_meta(self):
+        self.wgPreview.lblTitle.setText(self.file_name)
+        self.wgPreview.lblDate.setText(str(datetime.datetime.fromtimestamp(os.path.getmtime(self.load_file))).split(".")[0])
+        self.wgPreview.lblSize.setText(str("{0:.2f}".format(os.path.getsize(self.load_file)/(1024*1024.0)) + " MB"))
+    
+    
+    def clear_meta(self):
+        self.wgPreview.lblUser.setText('')
+        self.wgPreview.lblTitle.setText('')
+        self.wgPreview.lblDate.setText('')
+
+
+    ##Class:ArLoad************************************************************
+    # PRESS EVENT METHODS # 
+
     def press_btnAccept(self):
+
         if not os.path.exists(self.load_file):
             self.set_status('FAILED LOADING : Path doesn\'t exists: {}'.format(self.load_file), msg_type=3)
             return False
+        
+
     def press_menuItemAddFolder(self):
         import arSaveAs
         self.save_as = arSaveAs.start(new_file=False)
-    def press_menuSort(self, list_widget, reverse=False):
+
+
+    def press_menuSort(self, list_widget, reverse=False):  
         file_list = []
+
         for index in xrange(list_widget.count()):
              file_list.append(list_widget.item(index).text())
+        
         list_widget.clear()
         list_widget.addItems(sorted(file_list, reverse=reverse))
+
+
+    ##Class:ArLoad************************************************************
+    # CHANGE METHODS # 
+
     def change_lstScene(self):
+        """Description of change_lstScene method"""
         self.load_dir = self.data['project']['PATH'][self.wgLoad.lstScene.currentItem().text()]
         tmp_content = libFunc.get_file_list(self.load_dir)
         self.scene_steps = len(self.data['rules']['SCENES'][self.wgLoad.lstScene.currentItem().text()].split('/'))
+        
         if self.scene_steps < 5:
             self.wgLoad.lstAsset.hide()
         else:
             self.wgLoad.lstAsset.itemSelectionChanged.connect(self.change_lstAsset)
             self.wgLoad.lstAsset.show()
+
         self.wgLoad.lstSet.clear()
+        
         if tmp_content:
             self.wgLoad.lstSet.addItems(sorted(tmp_content))
             self.wgLoad.lstSet.setCurrentRow(0)
+    
+    
     def change_lstSet(self):
+        """Description of change_lstSet method"""
         new_path = self.load_dir + '/' + self.wgLoad.lstSet.currentItem().text()
         tmp_content = libFunc.get_file_list(new_path)
+        
         if self.scene_steps < 5:
             self.wgLoad.lstTask.clear()
+            
             if tmp_content:
                 self.wgLoad.lstTask.addItems(sorted(tmp_content))
                 self.wgLoad.lstTask.setCurrentRow(0)
         else:
             self.wgLoad.lstAsset.clear()
+            
             if tmp_content:
                 self.wgLoad.lstAsset.addItems(sorted(tmp_content))
                 self.wgLoad.lstAsset.setCurrentRow(0)
+    
+    
     def change_lstAsset(self):
+        """Description of change_lstAsset method"""
         new_path = self.load_dir + '/' + self.wgLoad.lstSet.currentItem().text() \
                    + '/' + self.wgLoad.lstAsset.currentItem().text()
         tmp_content = libFunc.get_file_list(new_path)
         self.wgLoad.lstTask.clear()
+        
         if tmp_content:
             self.wgLoad.lstTask.addItems(sorted(tmp_content))
             self.wgLoad.lstTask.setCurrentRow(0)
-    def fill_meta(self):
-        self.wgPreview.lblTitle.setText(self.file_name)
-        self.wgPreview.lblDate.setText(str(datetime.datetime.fromtimestamp(os.path.getmtime(self.load_file))).split(".")[0])
-        self.wgPreview.lblSize.setText(str("{0:.2f}".format(os.path.getsize(self.load_file)/(1024*1024.0)) + " MB"))
-    def clear_meta(self):
-        self.wgPreview.lblUser.setText('')
-        self.wgPreview.lblTitle.setText('')
-        self.wgPreview.lblDate.setText('')
+    
+
+
+
+#################################################################################
+# EXECUTION (START)
+
 def execute_the_class_ar_load():
     global main_widget
     main_widget = ArLoad()
